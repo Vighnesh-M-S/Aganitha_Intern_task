b239fc333a3a7ab9ea1b4cce9764c9fb4c941546
VIGHNESH M S <princevighnesh71@gmail.com>
2025-01-17 13:27:13 +0530

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="get-papers",
-    version="0.3.0",
+    version="0.3.1",
     description="A tool to fetch and process PubMed papers.",
     author="VIGHNESH M S",
     author_email="vighneshms21@gmail.com",
